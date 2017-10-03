98833e508c0dd7d34568c807120555a4d1233e8a
nayefahmad <nayef.ahmad.11@gmail.com>
2017-10-03 14:16:38 -0700

    app.R file for Hello Shiny example

---
+++ App-1/app.R
@@ -0,0 +1,80 @@
+
+#*****************************************
+# Playing with the Hello Shiny example
+#*****************************************
+
+library(shiny)
+
+
+
+
+# 1) Define UI for app that draws a histogram ----
+ui <- fluidPage(
+      
+      # App title ----
+      titlePanel("Hello World!"),
+      
+      # Sidebar layout with input and output definitions ----
+      sidebarLayout(
+            
+            # Sidebar panel for inputs ----
+            sidebarPanel(
+                  
+                  # Input: Slider for the number of bins ----
+                  sliderInput(inputId = "bins",
+                              label = "Number of bins:",
+                              min = 5,
+                              max = 50,
+                              value = 30)
+                  
+            ),
+            
+            # Main panel for displaying outputs ----
+            mainPanel(
+                  
+                  # Output: Histogram ----
+                  plotOutput(outputId = "distPlot")
+                  
+            )
+      )
+)
+
+
+
+# 2) Define server logic required to draw a histogram ----
+server <- function(input, output) {
+      
+      # Histogram of the Old Faithful Geyser Data ----
+      # with requested number of bins
+      # This expression that generates a histogram is wrapped in a call
+      # to renderPlot to indicate that:
+      #
+      # 1. It is "reactive" and therefore should be automatically
+      #    re-executed when inputs (input$bins) change
+      # 2. Its output type is a plot
+      output$distPlot <- renderPlot({
+            
+            x    <- faithful$waiting
+            bins <- seq(min(x), max(x), length.out = input$bins + 1)
+            
+            hist(x, breaks = bins, col = "#75AADB", border = "black",
+                 xlab = "Waiting time to next eruption (in mins)",
+                 main = "Histogram of waiting times")
+            
+      })
+      
+}
+
+
+# 3) Call to shinyApp: ----------------
+shinyApp(ui = ui, server = server)
+
+
+# 4) Run the app --------------
+# runApp("App-1")
+# paste this in console and then run, otherwise you get error: 
+# ERROR: Can't call `runApp()` from within `runApp()`. If your application code contains `runApp()`, please remove it.
+
+# alternative: 
+# use "Run app button", or press Control+Shift+Enter
+
