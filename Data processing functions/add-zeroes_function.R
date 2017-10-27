b7278eb36c9974dd5b17c9331c3a397aa346cd7d
nayefahmad <nayef.ahmad.11@gmail.com>
2017-10-26 19:25:27 -0700

    add data processing functions

---
+++ Data processing functions/add-zeroes_function.R
@@ -0,0 +1,40 @@
+
+#**********************************
+# Add zeroes in front of numbers to get a specified output length 
+#**********************************
+
+# create test data: -------- 
+# x <- c(2,400, 12001)
+
+# define function:---------
+add.zero.in.front <- function(input.number, output.length){
+      # inputs: a single number, number of zeroes to add in front 
+      # output: new number of desired total length 
+      
+      input.number <- as.character(input.number)
+      length <- nchar(input.number)
+      # print(length)
+      if (length < output.length) {
+            zeroes <- rep("0", times=(output.length-length)) %>% 
+                  paste(collapse="")
+            return(paste0(zeroes, input.number))
+      } else {
+            return(input.number)
+      }
+}
+      
+      
+# test the function: 
+# add.zero.in.front(1,5)
+# lapply(x, add.zero.in.front, output.length=5)
+
+
+#******************************************************
+# test with numbers from Excel: ----------
+# input.numbers <- readClipboard()  # copy numbers from excel
+# output.numbers <- sapply(input.numbers, add.zero.in.front, output.length=5)
+# 
+# write.table(output.numbers, file="clipboard", sep="\t",  # copy back to Excel
+#              row.names=FALSE,
+#              col.names = FALSE)
+
---
+++ Data processing functions/add.quotes_function.R
@@ -0,0 +1,33 @@
+
+
+#**********************************
+# Add quote marks before and after numbers to allow pasting in SQL
+#**********************************
+
+# test data: 
+# x <- c(50, 75)
+
+# define fun: 
+add.quotes <- function(input.number, isInsertedIntoSqltable){
+      if (isInsertedIntoSqltable == FALSE) {
+            paste0("'", input.number, "', ")
+      } else {
+            paste0("('", input.number, "'), ")
+      }
+      
+}
+
+# test fun: 
+# sapply(x, add.quotes, isInsertedIntoSqltable=FALSE)
+# sapply(x, add.quotes, isInsertedIntoSqltable=TRUE)
+
+# **********************************************
+# test with numbers from Excel: ------
+input.numbers <- readClipboard()  # copy numbers from excel
+output.numbers <- sapply(input.numbers, 
+                         add.quotes, 
+                         isInsertedIntoSqltable=TRUE) %>% as.data.frame
+
+write.table(output.numbers, file="clipboard", sep="\t",  # copy to Excel
+            row.names=FALSE,
+            col.names = FALSE)
