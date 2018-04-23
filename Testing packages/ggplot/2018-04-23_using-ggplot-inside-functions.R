cc54ec1be5520dd1d1b469c9cf0dc9f2771ec6b8
nayefahmad <nayef.ahmad.11@gmail.com>
2018-04-23 16:43:53 -0700

    using ggplot inside functions: aes_string( ) function

---
+++ Testing packages/ggplot/2018-04-23_using-ggplot-inside-functions.R
@@ -0,0 +1,49 @@
+
+
+#************************************************************
+# USING GGPLOT INSIDE FUNCTIONS
+#************************************************************
+
+library("dplyr")
+library("magrittr")
+library("ggplot2")
+
+
+
+
+#***************************************************************
+# 1. aes_string : ----------------------------------------------
+#***************************************************************
+
+# What if you don't want to specify ahead of time which cols will be on 
+#     x and y axes? 
+
+# fn to plot any 2 cols as scatter: 
+plot_fn <- function(df, x.axis, y.axis) {
+      ggplot(df, 
+             aes(x = x.axis, 
+                 y = y.axis)) + 
+            
+            geom_point()
+}
+
+
+# let's try out the fn: 
+plot_fn(mtcars, "mpg", "cyl")
+# this isn't what we wanted. ggplot is not looking at the mtcars dataset, it's just 
+#     treating "mpg" and "cyl" as 2 strings
+
+
+# fn with aes_string: 
+plot.fn.with.aes <- function(df, x.axis, y.axis, point.colour = NULL) {
+      ggplot(df, 
+             aes_string(x = x.axis, 
+                        y = y.axis, 
+                        colour = point.colour)) + 
+            
+            geom_point()
+}
+
+# test the function: 
+plot.fn.with.aes(mtcars, "mpg", "cyl")
+plot.fn.with.aes(mtcars, "mpg", "cyl", "gear")
