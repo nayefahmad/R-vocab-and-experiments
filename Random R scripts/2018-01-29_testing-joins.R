5e35258990a6fb31334b748c1e1fd27468e6bf02
nayefahmad <nayef.ahmad.11@gmail.com>
2018-04-25 16:32:02 -0700

    testing joins: wrong way to calculate utilization

--- Random R scripts/2018-01-29_testing-joins.R
+++ Random R scripts/2018-01-29_testing-joins.R
@@ -13,12 +13,36 @@ df1 <- data.frame(lha=c("ubc",
                         "uptwn", 
                         "uptwn"), 
                   gender= rep(c("m", "f"), times=6), 
-                  ctas=sample(5, 12, replace = TRUE))  %>% print 
+                  ctas=sample(5, 12, replace = TRUE), 
+                  visits = rpois(nrow(df1), 500))  %>% print 
 
 df2 <- data.frame(lha=c("ubc", "downt", "uptwn"), 
                   population=rpois(3, 20)) %>% print 
 
 
 # inner join: 
-inner_join(df1, df2)
+inner_join(df1, df2) %>% arrange(lha)
 
+# left join: 
+left_join(df1, df2) %>% arrange(lha)
+
+# Any difference? No. 
+identical(inner_join(df1, df2), left_join(df1, df2))  # TRUE 
+
+
+# inner join and group by: ---------------------------------------------------
+inner_join(df1, df2) %>% 
+      group_by(lha, gender, ctas) %>% 
+      mutate(util = visits/population,  # correct way to calculate utilization 
+             
+             # following is the incorrect way: 
+             sum.visits = sum(visits), 
+             sum.pop = sum(population), 
+             util2 = sum.visits/sum.pop, 
+             is.util.equal.util2 = ifelse(util == util2, 
+                                          TRUE, 
+                                          FALSE)) %>% 
+      arrange(lha)
+
+# Note: difference between util and util2 is because variable visits 
+#     is excluded from the group by statement
