5f52ad0c893db50175888d163d18a35aee8fd211
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2018-07-03 16:34:11 -0700

    tree to find most significant split with single predictor

--- 2018-07-03_visualizing-trees-from-random-forest.R
+++ 2018-07-03_visualizing-trees-from-random-forest.R
@@ -6,6 +6,7 @@
 # Created: 2018-07-03
 
 library("randomForest")
+library("ggplot2")
 # library("broom")  # can't deal with random forest :( 
 
 # help(package = "randomForest")
@@ -30,6 +31,7 @@ mtcars.rforest <- randomForest(mpg ~ .,
 # plotting errors and var importance as trees grow: -----------------
 plot(mtcars.rforest)
 plot(mtcars.rforest, log = "y")  # not sure what the point of log is 
+# error units in miles per gallon? 
 
 # ?varImpPlot
 varImpPlot(mtcars.rforest)
@@ -50,11 +52,33 @@ getTree(mtcars.rforest,
         labelVar = TRUE)
 
 # A matrix (or data frame, if labelVar=TRUE) with six columns and number of rows equal 
-# to total number of nodes in the tree. 
-# 
+# to total number of nodes in the tree. For numerical predictors, data with 
+# values of the variable less than or equal to the splitting point go to the 
+# left daughter node.
+
 # left daughter =  the row where the left daughter node is; 0 if the node is terminal
 # right daughter	=  the row where the right daughter node is; 0 if the node is terminal
 # split var	=  which variable was used to split the node; 0 if the node is terminal
 # split point = where the best split is; see Details for categorical predictor
 # status = is the node terminal (-1) or not (1)
 # prediction = the prediction for the node; 0 if the node is not terminal
+
+
+# using only 1 preditor: ---------------
+mtcars.rforest2 <-  randomForest(mpg ~ wt, 
+                                 data = mtcars, 
+                                 ntree = 1000, 
+                                 keep.forest = TRUE, 
+                                 importance = TRUE)
+plot(mtcars.rforest2)
+getTree(mtcars.rforest2, 
+        sample(1:1000,1), 
+        labelVar = TRUE)
+
+# repeat step above several times, see if first split is always around 
+# the same value. Todo: write function/loop for this
+
+# Ans. First split is usually between wt = 2 and wt = 4 
+
+qplot(x = wt, y = mpg, data = mtcars) + theme_classic()
+
