33e560e4650ed8cfd34c1056129cf0463bfc7ff8
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2018-11-25 00:29:20 -0800

    trying to project new data onto principal subspace

--- 2018-11-24_pca-on-mtcars-data.R
+++ 2018-11-24_pca-on-mtcars-data.R
@@ -40,7 +40,7 @@ mtcars.pca <- prcomp(df1.mtcars.subset,
 summary(mtcars.pca)
 # first PC explains 62% of variance
 
-print(mtcars.pca)
+print(mtcars.pca)  # todo: are these the factor loadings? 
 
 plot(mtcars.pca)
 
@@ -74,11 +74,33 @@ p1.mtcars.pc1 <-
 
 
 
+# biplots from the PCA result: -----
+?biplot
+biplot(mtcars.pca)
+
+
+# Project new data onto the new basis: -----
+new.car <- data.frame(mpg = 10, 
+                      cyl = 10, 
+                      disp = 1000, 
+                      hp = 400, 
+                      drat = 5, 
+                      wt = 2, 
+                      qsec = 17, 
+                      gear = 5, 
+                      carb = 8, 
+                      car = "newcar")
+
+# todo: why not working? 
+new.car[ ,-10] %>% 
+      t %>% 
+      scale(center = mtcars.pca$center)
 
 
 
-
-# save outputs 
+#*************************************************
+# save outputs --------
+#*************************************************
 ggsave(here("2018-11-24_mtcars-first-PC.pdf"), 
        p1.mtcars.pc1,
        width = 10)
