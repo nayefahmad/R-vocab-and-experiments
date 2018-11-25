320ca7bf056374a98693a9a8231e85587adcfd7b
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2018-11-25 01:18:04 -0800

    plot first 2 PCs, try plotting new data point projected onto the space

--- 2018-11-24_pca-on-mtcars-data.R
+++ 2018-11-24_pca-on-mtcars-data.R
@@ -17,12 +17,10 @@ library(here)
 # PCA works best with numeric data
 df1.mtcars.subset <- 
       mtcars %>% 
-      select(-c(vs, am)) %>% 
-      bind_cols(car = rownames(mtcars))
-
+      select(-c(vs, am)) 
 
 # PCA with prcomp( ) function: ------
-?prcomp
+# ?prcomp
 
 mtcars.pca <- prcomp(df1.mtcars.subset, 
                      center = TRUE, 
@@ -36,12 +34,19 @@ mtcars.pca <- prcomp(df1.mtcars.subset,
 
 # Examining the PCA object: --------
 # str(mtcars.pca)  # not useful 
-
 summary(mtcars.pca)
 # first PC explains 62% of variance
 
-print(mtcars.pca)  # todo: are these the factor loadings? 
+# components withing the pca "model" object: 
+names(mtcars.pca)
+mtcars.pca$rotation  # gives the loadings 
+mtcars.pca$center  # gives means of variables; check by calling 
+                   # summary(df1.mtcars.subset)
+mtcars.pca$scale
+mtcars.pca$x  # same output as tidy(mtcars.pca), but in wide format
 
+# print and plot: 
+print(mtcars.pca)  # todo: are these the factor loadings? 
 plot(mtcars.pca)
 
 tidy(mtcars.pca)  # %>% View("tidy(PCA)")
@@ -52,15 +57,28 @@ tidy(mtcars.pca)  # %>% View("tidy(PCA)")
 
 
 
-# let's plot the cars on PC1: 
+# let's plot the cars on PC1: ----
+df2.carnames <- rownames(mtcars) %>% 
+      as.data.frame() %>% 
+      rename(car = ".")
+
 p1.mtcars.pc1 <- 
       tidy(mtcars.pca) %>% 
-      filter(PC == 1) %>% 
-      left_join(df1.mtcars.subset, 
-                by = c("row" = "car")) %>% 
+      
+      # choose PC1 only: 
+      filter(PC == 1) %>%
+      
+      # add car names back: 
+      left_join(df2.carnames, 
+                by = c("row" = "car")) %>%
+      
+      # now cbind on full mtcars to get all vars: 
+      bind_cols(df1.mtcars.subset) %>% 
+      
       mutate(y = rep(1, 32), 
              cyl = as.factor(cyl)) %>%
       
+      # plotting: 
       ggplot(aes(x = value, 
                  y = y, 
                  col = cyl, 
@@ -78,6 +96,25 @@ p1.mtcars.pc1 <-
 ?biplot
 biplot(mtcars.pca)
 
+# using ggplot: 
+p2.mtcars.2components <- 
+      mtcars.pca$x %>%  
+      as.data.frame() %>% 
+      bind_cols(df2.carnames) %>% 
+      bind_cols(df1.mtcars.subset) %>% 
+      mutate(cyl = as.factor(cyl)) %>% 
+      
+      ggplot(aes(x = PC1, 
+                 y = PC2, 
+                 label = car, 
+                 col = cyl, 
+                 size = hp)) + 
+      geom_point() + 
+      geom_text_repel(force = 5, 
+                      size = 4); p2.mtcars.2components
+
+
+
 
 # Project new data onto the new basis: -----
 new.car <- data.frame(mpg = 10, 
@@ -88,13 +125,42 @@ new.car <- data.frame(mpg = 10,
                       wt = 2, 
                       qsec = 17, 
                       gear = 5, 
-                      carb = 8, 
-                      car = "newcar")
+                      carb = 8)
 
-# todo: why not working? 
-new.car[ ,-10] %>% 
-      t %>% 
+# mean-center the new car:  
+new.car.centered <- 
+      new.car %>% 
       scale(center = mtcars.pca$center)
+
+# rotate to the new basis for the vector space: 
+new.car.projected <- 
+      new.car.centered %*% 
+      mtcars.pca$rotation %>% 
+      as.data.frame()
+# this gives us the coordinates of new.car along 
+# the new basis vectors of the space. 
+
+
+# plot PC1 with the new car added: 
+
+
+
+p3.mtcars.pca.with.newcar <- 
+      mtcars.pca$x %>% 
+      rbind(new.car.projected) %>% 
+      as.data.frame %>% 
+      mutate(car = rownames(.)) %>% 
+      mutate(car = ifelse(car == "1",
+                          "NEWCAR", 
+                          car)) %>% 
+      
+      # now plot it: 
+      ggplot(aes(x = PC1, 
+                 y = rep(1, 33), 
+                 label = car)) +
+      geom_point(); p3.mtcars.pca.with.newcar
+      
+      
 
 
 
