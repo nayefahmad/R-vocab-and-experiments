326b4a4e8a7b0cee343ff334c4c40511f3610890
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-05-02 17:05:39 -0700

    NEW: Testing ODBC connection to denodo

---
+++ 2019-05-02_testing-denodo-ODBC-connection.R
@@ -0,0 +1,12 @@
+
+#*******************************************************************
+# Testing ODBC connection to denodo 
+# 2019-05-02
+# Nayef 
+
+#*******************************************************************
+
+library(odbc)
+library(dbplyr)
+library(magrittr)  # not really essential 
+library(tidyverse)
