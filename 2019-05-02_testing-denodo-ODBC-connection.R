9289e548417c0d31910d3de764a04123570826ee
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-05-02 17:31:51 -0700

    connect to denodo publish: SUCCESS!

--- 2019-05-02_testing-denodo-ODBC-connection.R
+++ 2019-05-02_testing-denodo-ODBC-connection.R
@@ -10,3 +10,25 @@ library(odbc)
 library(dbplyr)
 library(magrittr)  # not really essential 
 library(tidyverse)
+
+
+# References: ---------
+# > also see file "2018-10-09_dbplyr-package-test-drive.R"
+# > https://community.denodo.com/tutorials/browse/basics/4connect2odbcclient
+
+
+# 1) set up database connection: -----------
+# Note that we need a 64-bit ODBC connection, so in Windows start menu, use
+# "ODBC Data Sources (64-bit)"
+
+cnx <- dbConnect(odbc::odbc(),
+                 dsn = "denodo_test")
+
+
+
+
+
+
+
+
+
