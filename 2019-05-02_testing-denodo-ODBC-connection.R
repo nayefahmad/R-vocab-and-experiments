3dcc10f75f1ac931a298c700e1d16fab00e546c7
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-05-02 18:08:57 -0700

    Examples of ED and ADTC queries from denodo publish

--- 2019-05-02_testing-denodo-ODBC-connection.R
+++ 2019-05-02_testing-denodo-ODBC-connection.R
@@ -22,13 +22,62 @@ library(tidyverse)
 # "ODBC Data Sources (64-bit)"
 
 cnx <- dbConnect(odbc::odbc(),
-                 dsn = "denodo_test")
+                 dsn = "cnx_denodo_spappcsta001")
 
 
 
 
 
+# 2) connect to view: ----------
+vw_eddata <- dplyr::tbl(cnx, 
+                     dbplyr::in_schema("publish", 
+                                       "emergency"))
 
+# str(vw_eddata)
+
+vw_adtc <- dplyr::tbl(cnx, 
+                   dbplyr::in_schema("publish", 
+                                     "admission_discharge"))
+# str(vw_adtc)
+
+
+
+
+
+# 3) example ED query: ----------
+# show the sql syntax: 
+vw_eddata %>% 
+    filter(facility_name == 'Lions Gate Hospital', 
+           start_date_id == '20190501') %>% 
+    select(patient_id, 
+           is_admitted) %>%
+    show_query()
+
+
+# collect results from the database: 
+df1.visits <- 
+    vw_eddata %>% 
+    filter(facility_name == 'Lions Gate Hospital', 
+           start_date_id == '20190501') %>% 
+    select(patient_id, 
+           is_admitted) %>%
+    collect()
+
+# df1.visits 
+
+
+
+
+# 4) example adtc query: -----
+df2.admits <- 
+    vw_adtc %>% 
+    filter(facility_name == 'Lions Gate Hospital', 
+           admission_date_id == '20190501') %>% 
+    select(patient_id, 
+           nursing_unit_at_admit) %>% 
+    collect()
+
+# df2.admits
 
 
 
