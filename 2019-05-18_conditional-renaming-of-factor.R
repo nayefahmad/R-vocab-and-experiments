81c088db76fff96546973762f2d8928bfa896e69
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-05-18 18:22:04 -0700

    inelegant solution: convert to char, then back to factor

--- 2019-05-18_conditional-renaming-of-factor.R
+++ 2019-05-18_conditional-renaming-of-factor.R
@@ -9,19 +9,42 @@
 library(tidyverse)
 
 # Data: -------------
+set.seed(1)
+
 df <- data.frame(fruit = sample(c("apple", 
-                                  "banana"), 
+                                  "banana", 
+                                  "cherry"), 
                                 10, 
                                 replace = TRUE)) %>% 
     mutate(colour = case_when(fruit == "apple" ~ "red", 
-                              fruit == "banana" ~ "yellow") %>% 
+                              fruit == "banana" ~ "yellow", 
+                              fruit == "cherry" ~ "red") %>% 
                as.factor())
 
 str(df)
-
+head(df, 10)
 
 # Problem statement: ------------
-# Change colour to green when fruit is apple. Don't convert
-# fruit to a char, then convert back. The point is to find a
-# better way than that.
+# Change colour to green when fruit is apple (but leave
+# cherry as is). DON'T convert fruit to a char, then convert
+# back. The point is to find a better way than that.
+
+
+
+# Solution: ---------------
+
+# help(package = "forcats")
+# ?fct_recode
+# ?fct_match
+# ?replace
+
+# This works, but isn't very elegant bcoz requires
+# converting colour to character then back to factor: 
+df <- df %>% 
+    mutate(colour = case_when(fruit == "apple" ~ "green", 
+                              TRUE ~ as.character(colour)) %>% factor)
+
+str(df)
+head(df, 10)
+
 
