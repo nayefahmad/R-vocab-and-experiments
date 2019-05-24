ca81c106b18f7432bc9f74c1082953e8cf1d577a
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-05-24 16:35:34 -0700

    NEW: Tidy forecasting with `sweep` and `fable`

---
+++ 2019-05-24_tidy-forecasting-with-sweep-and-fable.R
@@ -0,0 +1,10 @@
+
+#'--- 
+#' title: "Tidy forecasting with `sweep` and `fable`"
+#' author: "Nayef Ahmad"
+#' date: "May 24, 2019"
+#' output: html_document
+#' ---
+
+#' Reference: https://www.youtube.com/watch?v=nERXS3ssntw
+
