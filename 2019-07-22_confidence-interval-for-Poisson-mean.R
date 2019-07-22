f87a5fb7fabaea69b1e8820be0720c8afd578b47
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-07-22 16:14:14 -0700

    New: Confidence interval for a Poisson mean

---
+++ 2019-07-22_confidence-interval-for-Poisson-mean.R
@@ -0,0 +1,53 @@
+
+
+#'--- 
+#' title: "Confidence interval for a Poisson mean"
+#' author: "Nayef Ahmad"
+#' date: "2019-07-22"
+#' output: 
+#'   html_document: 
+#'     keep_md: yes
+#'     code_folding: hide
+#'     toc: true
+#'     toc_float: true
+#' ---
+#' 
+#' 
+
+#+ lib, include = FALSE 
+library(tidyverse)
+library(kableExtra)
+
+
+#' ## Data
+
+#+ analysis 
+df1.data <- 
+  tibble::tribble(
+    ~fiscal_year, ~site, ~discharges, ~in_hospital_deaths,
+         "16/17", "VGH",        4288,                  32,
+         "17/18", "VGH",        4476,                  31,
+         "18/19", "VGH",        4450,                  39
+    )
+
+
+df1.data %>% 
+  mutate(lambda_hat = in_hospital_deaths/discharges) %>%  
+  kable() %>% 
+  kable_styling(bootstrap_options = c("striped",
+              "condensed", 
+              "responsive"))
+
+
+#' ## Confidence interval for Poisson mean Reference:
+#' * **Using normal approx**: https://stats.stackexchange.com/questions/15371/how-to-calculate-a-confidence-level-for-a-poisson-distribution
+#' * **Without normal approx**: https://en.wikipedia.org/wiki/Poisson_distribution#Confidence_interval
+#'
+#' 
+
+
+
+    
+  
+
+
