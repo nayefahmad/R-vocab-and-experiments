f995d60c14c3cd57251a48810099640ab5a21672
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-08-30 19:58:01 -0700

    FAIL; can't install {fable}

--- 2019-08-30_tidy-forecasting-with-tsibble-feasts-fable-etc.R
+++ 2019-08-30_tidy-forecasting-with-tsibble-feasts-fable-etc.R
@@ -11,16 +11,26 @@
 #'     toc_float: true
 #' ---
 
+#' References: 
+#' 
+#' * https://tidyverts.org/ 
+#' * https://github.com/business-science/sweep
+#' 
 
 
 
-
+#+ lib, include = FALSE 
 library(magrittr)
 library(ggplot2)
 library(feasts)
+# library(fable)  todo: FAILs to install
 
+#+ rest 
+#' To begin, we'll simulate a random walk, then plot and analyze it using
+#' `tsibble`, `feasts`, and `fable` packages
+#' 
 
-# function for a random walk: 
+# function for a random walk: ----------
 random_walk <- function(t = 100,
                         drift = 0,
                         sd_white_noise = .5){
@@ -31,24 +41,38 @@ random_walk <- function(t = 100,
 }
 
 
-
-# simulate the random walk 
+#+ 
+# simulate the random walk: ---------
 df1 <- data.frame(time = 1:100, 
                   value = random_walk())
-df1 %>% 
-  ggplot(aes(x = time, 
-             y = value)) + 
-  geom_line() + 
-  scale_y_continuous(limits = c(-10, 10))
-
-
 
 ts1 <- df1 %>% as_tsibble(index = time)
+autoplot(ts1)
 
 
+#' Can STL detect that there's no seasonality? 
+#' 
+#' Yes! 
 
-
+# decomposition: --------------
 STL(ts1) %>% autoplot()
+
+
+
+#' Now let's fit a model 
+#' 
+
+# model fitting: ---------
+
+ts1 %>% 
+  model(RW(value ~ drift()))
+  
+
+
+
+
+
+
 
 
 
