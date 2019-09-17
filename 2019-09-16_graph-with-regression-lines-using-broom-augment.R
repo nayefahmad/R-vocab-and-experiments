f276002f3e69d57f40dcc5510f72206ab9ec479c
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-09-16 17:19:13 -0700

    Create 2019-09-16_graph-with-regression-lines-using-broom-augment.R

---
+++ 2019-09-16_graph-with-regression-lines-using-broom-augment.R
@@ -0,0 +1,30 @@
+
+
+library(tidyverse)
+library(broom)
+
+mtcars.modified <- 
+  mtcars %>% 
+  mutate(cyl = as.factor(cyl))
+
+
+m1 <- lm(mpg ~ hp + cyl, 
+         data = mtcars.modified)
+
+m1 %>% 
+  broom::augment() %>% 
+  
+  ggplot() + 
+  
+  # add the fitted regresseion lines: 
+  geom_line(aes(x = hp, 
+                y = .fitted, 
+                group = cyl, 
+                col = cyl)) + 
+  
+  # add the actual y-values
+  geom_point(data = mtcars.modified, 
+             aes(x = hp, 
+                 y = mpg, 
+                 col = cyl), 
+             alpha = .2)
