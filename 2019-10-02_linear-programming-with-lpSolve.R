775dc03b7b8b5aee69b2f52ae9cdbfbaa63f1230
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-10-02 13:18:04 -0700

    add notes

--- 2019-10-02_linear-programming-with-lpSolve.R
+++ 2019-10-02_linear-programming-with-lpSolve.R
@@ -11,10 +11,43 @@
 #'     toc_float: true
 #' ---
 
+#' Reference: https://www.r-bloggers.com/linear-programming-in-r/
+#'
+#' # Problem Statement
+#'
+#' "This is an example of linear optimization that I found in the book “Modeling
+#' and Solving Linear Programming with R” by Jose M. Sallan, Oriol Lordan and
+#' Vincenc Fernandez.  The example is named “Production of two models of chairs”
+#' and can be found at page 57, section 3.5. I’m going to solve only the first
+#' point."
+#'
+#'
+#' > A company produces two models of chairs: 4P and 3P. The model 4P needs 4
+#' legs, 1 seat and 1 back. On the other hand, the model 3P needs 3 legs and 1
+#' seat. The company has a initial stock of 200 legs, 500 seats and 100 backs.
+#' If the company needs more legs, seats and backs, it can buy standard wood
+#' blocks, whose cost is 80 euro per block. The company can produce 10 seats, 20
+#' legs and 2 backs from a standard wood block. The cost of producing the model
+#' 4P is 30 euro/chair, meanwhile the cost of the model 3P is 40 euro/chair.
+#' Finally, the company informs that the minimum number of chairs to produce is
+#' 1000 units per month. Define a linear programming model, which minimizes the
+#' total cost (the production costs of the two chairs, plus the buying of new
+#' wood blocks).
+#' 
+#' 
 
+
+
+
+#+ lib, include = FALSE 
 # Load lpSolve
-require(lpSolve)
+library(lpSolve)
+library(lpSolveAPI)
 
+
+#' # Solution with lpSolve
+# Solution with lpSolve -----------
+#+ rest 
 ## Set the coefficients of the decision variables -> C
 C <- c(30, 40, 80)
 
@@ -38,6 +71,8 @@ optimum <-  lp(direction="min",
                const.rhs = B,
                all.int = T)
 
+str(optimum)
+
 # Print status: 0 = success, 2 = no feasible solution
 print(optimum$status)
 
@@ -49,9 +84,9 @@ print(best_sol)
 # Check the value of objective function at optimal point
 print(paste("Total cost: ", optimum$objval, sep=""))
 
-#################
+#*********************************************************
 #   Output      #
-#################
+#*********************************************************
 
 # [1] 0
 # x_4p x_3p  x_w 
@@ -61,7 +96,10 @@ print(paste("Total cost: ", optimum$objval, sep=""))
 
 rm(optimum, constranints_direction, best_sol)
 
-#-------------------------------------------------------------------------------
+#*********************************************************
+
+#' # Solution with lpSolveAPI
+# Solution with lpSolveAPI -------------------
 # Let's try to solve the problem again using lpSolveAPI
 
 # Use lpSolveAPI
@@ -100,9 +138,9 @@ get.bounds(lprec)
 # Boundaries can be set with following function
 #lpSolveAPI::set.bounds()
 
-#################
+#*********************************************************
 #   Output      #
-#################
+#*********************************************************
 
 # [1] 420 580 161
 # [1] 48680
